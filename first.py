922e86f5f9214d9e280e1f98bf9fdc807717db5e
mustfa-magdy-mohamed-nasr <Mostfammaaggddyy@gmail.com>
2024-01-06 17:31:05 +0200

    taskW2

---
+++ first.py
@@ -0,0 +1,44 @@
+# # print('I love Python')
+# print("""
+# Hallo1
+      
+
+# # I"am"
+# # mustfa
+# # # """)
+# # myString='I Love Python'
+# # print(myString[-1])
+# # print(myString[::3])
+# str='####I love Python          '
+# # print(len(str))
+# print(str.rstrip("#"))
+# # print(str.rstrip())
+# # print(str.lstrip())
+#"Hello 'Osama', How You Doing \ """ Your Age Is "38"" + And Your Country Is: Egypt
+#Hello'mutfa', How You Doing \ """ Your Age Is"20""+And Your Country Is:Egypt
+# name='mutfa'
+# adg='20'
+# cuntry='Egypt'
+# # print('Hello\''+name+'\', How You Doing \\ \"\"\" Your Age Is"'+adg+'""+And Your Country Is:'+cuntry)
+# # #"Hello 'Osama', How You Doing \
+# # """ Your Age Is "38"" +
+# # And Your Country Is: Egypt
+# print('Hello\''+name+'\', How You Doing \\ \n\"\"\" Your Age Is"'+adg+'""+\nAnd Your Country Is:'+cuntry)
+name = 'Elzero'
+mo='mm'
+
+# # Needed Output
+# # Second Letter Is "l"
+# # Third Letter Is "z"
+# # Last Letter Is "o"
+# print(name[1])
+# print(name[2])
+# print(name[-1])
+# Needed Output
+# "lze"
+# "Ezr"
+# "rzE"
+print(name[1:4])
+print(name[0:5:2])
+print(name[-2::-2])
+
